675cb6eea9a90479eb0b2a7cde6d6e7e97388bf2
AppleTV <2756092+chanomie@users.noreply.github.com>
2018-10-31 15:27:07 -0700

    Updated by making sure a full path is used for files

--- rss/dvdsreleasedates-rss.sh
+++ rss/dvdsreleasedates-rss.sh
@@ -2,15 +2,16 @@
 
 # Requires: brew install tidy-html5
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 DOWNLOAD_URL="https://www.dvdsreleasedates.com"
 DOWNLOAN_FILE="dvdsreleasedates"
 
-curl -s ${DOWNLOAD_URL} > ${DOWNLOAN_FILE}.html
-sed -i.bak 's/addthis://g' ${DOWNLOAN_FILE}.html
-tidy --vertical-space no -b -q -i -asxml ${DOWNLOAN_FILE}.html > ${DOWNLOAN_FILE}.tidy.html 2>/dev/null
-xsltproc dvdsreleasedates.xslt ${DOWNLOAN_FILE}.tidy.html
+curl -s ${DOWNLOAD_URL} > ${DIR}/${DOWNLOAN_FILE}.html
+sed -i.bak 's/addthis://g' ${DIR}/${DOWNLOAN_FILE}.html
+tidy --vertical-space no -b -q -i -asxml ${DIR}/${DOWNLOAN_FILE}.html > ${DIR}/${DOWNLOAN_FILE}.tidy.html 2>/dev/null
+xsltproc ${DIR}/dvdsreleasedates.xslt ${DIR}/${DOWNLOAN_FILE}.tidy.html
 
 ## Cleanup
-rm ${DOWNLOAN_FILE}.html
-rm ${DOWNLOAN_FILE}.html.bak
-rm ${DOWNLOAN_FILE}.tidy.html
+rm ${DIR}/${DOWNLOAN_FILE}.html
+rm ${DIR}/${DOWNLOAN_FILE}.html.bak
+rm ${DIR}/${DOWNLOAN_FILE}.tidy.html
